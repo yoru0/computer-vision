a8586ee7efc150f6f183a8ee0b0a3a63cbdb6770
yoru <jordanchandra1@gmail.com>
2025-09-14 22:11:02 +0700

    Initial commit

---
+++ 1/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread("image.jpg")
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+height, width, _ = image.shape
+image[:, width//2:] = (0, 255, 255)
+
+cv2.imwrite("modified_image.jpg", image)
