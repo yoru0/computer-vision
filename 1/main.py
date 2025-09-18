99301a91ca210c5462ad70dca8787eb712a5c930
yoru <jordanchandra1@gmail.com>
2025-09-18 15:39:02 +0700

    .

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread("image.jpg")
+image = cv2.imread("odi.jpg")
 
 cv2.imshow("Image", image)
 cv2.waitKey(0)
@@ -9,4 +9,4 @@ cv2.destroyAllWindows()
 height, width, _ = image.shape
 image[:, width//2:] = (0, 255, 255)
 
-cv2.imwrite("modified_image.jpg", image)
+cv2.imwrite("modified_odi.jpg", image)
