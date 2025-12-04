6c97553a76e8a4f38d12960d5a3ffb38fb78bbba
yoru0 <jordanchandra1@gmail.com>
2025-12-04 15:58:50 +0700

    .

--- 10_object_detection/main.py
+++ 10_object_detection/main.py
@@ -2,13 +2,13 @@ import cv2
 import os
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Face Recognition (+ Training)
 face_list = []
 label_list = []
 
-train_path = './dataset/train'
+train_path = "./dataset/train"
 person_names = os.listdir(train_path)
 
 for idx, name in enumerate(person_names):
@@ -17,13 +17,20 @@ for idx, name in enumerate(person_names):
         file_path = f"{dir_name}/{file_name}"
         img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 
-        detected_faces = face_cascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=5)
+        detected_faces = face_cascade.detectMultiScale(
+            img, scaleFactor=1.2, minNeighbors=5
+        )
         if len(detected_faces) < 1:
             continue
 
         for face in detected_faces:
             x, y, h, w = face
-            face_img = img[y:y+h, x:x+w]
-            
+            face_img = img[y : y + h, x : x + w]
+
             face_list.append(face_img)
             label_list.append(idx)
+
+
+# Face Detection (Testing)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.train(face_list, np.array(label_list))
